1dec6a0c12aacfb1341026562a5b7f6b3ed72e8f
frank <franktranvantu@gmail.com>
2023-06-09 02:47:03 +0700

    Update

--- fraud/src/main/java/com/franktran/fraud/FraudController.java
+++ fraud/src/main/java/com/franktran/fraud/FraudController.java
@@ -1,6 +1,7 @@
 package com.franktran.fraud;
 
 import com.franktran.domain.FraudCheckResponse;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,9 +9,11 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("api/v1/fraud-check")
+@Slf4j
 public record FraudController(FraudCheckService service) {
   @GetMapping("{customerId}")
   public FraudCheckResponse isFraudster(@PathVariable Integer customerId) {
+    log.info("Fraud check request for customer {}", customerId);
     boolean isFraudulentCustomer = service.isFraudulentCustomer(customerId);
     return new FraudCheckResponse(isFraudulentCustomer);
   }
