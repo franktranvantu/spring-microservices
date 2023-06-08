f2c984f5f0c106e0ff60491a992b1ca4c4e6c1a3
frank <franktranvantu@gmail.com>
2023-06-09 02:35:08 +0700

    9 - RestTemplate

--- fraud/src/main/java/com/franktran/fraud/FraudController.java
+++ fraud/src/main/java/com/franktran/fraud/FraudController.java
@@ -1,15 +1,15 @@
 package com.franktran.fraud;
 
 import com.franktran.domain.FraudCheckResponse;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("api/v1/fraud-check")
 public record FraudController(FraudCheckService service) {
-  @PostMapping("{customerId}")
+  @GetMapping("{customerId}")
   public FraudCheckResponse isFraudster(@PathVariable Integer customerId) {
     boolean isFraudulentCustomer = service.isFraudulentCustomer(customerId);
     return new FraudCheckResponse(isFraudulentCustomer);
