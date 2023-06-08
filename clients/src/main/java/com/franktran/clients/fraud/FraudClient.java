1c32634d3b118eead6566ebafbd8200865af3781
frank <franktranvantu@gmail.com>
2023-06-09 03:59:38 +0700

    Update

---
+++ clients/src/main/java/com/franktran/clients/fraud/FraudClient.java
@@ -0,0 +1,12 @@
+package com.franktran.clients.fraud;
+
+import com.franktran.domain.FraudCheckResponse;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@FeignClient("fraud")
+public interface FraudClient {
+  @GetMapping("api/v1/fraud-check/{customerId}")
+  FraudCheckResponse isFraudster(@PathVariable Integer customerId);
+}
