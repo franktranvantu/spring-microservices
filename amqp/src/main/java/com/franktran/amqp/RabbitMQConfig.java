dbe68664e63b0881ac7faab787fa0faab129341a
frank <franktranvantu@gmail.com>
2023-06-09 23:09:39 +0700

    Update

---
+++ amqp/src/main/java/com/franktran/amqp/RabbitMQConfig.java
@@ -0,0 +1,27 @@
+package com.franktran.amqp;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.amqp.core.AmqpTemplate;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.amqp.support.converter.MessageConverter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@RequiredArgsConstructor
+public class RabbitMQConfig {
+  private final ConnectionFactory connectionFactory;
+  @Bean
+  public AmqpTemplate amqpTemplate() {
+    RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory);
+    rabbitTemplate.setMessageConverter(messageConverter());
+    return rabbitTemplate;
+  }
+
+  @Bean
+  public MessageConverter messageConverter() {
+    return new Jackson2JsonMessageConverter();
+  }
+}
