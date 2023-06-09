fef9161b9c1bc7f6a4d6bcfec5309c215fe8656b
frank <franktranvantu@gmail.com>
2023-06-09 23:31:59 +0700

    Update

---
+++ notification/src/main/java/com/franktran/notification/NotificationConfig.java
@@ -0,0 +1,16 @@
+package com.franktran.notification;
+
+import lombok.Getter;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@Getter
+public class NotificationConfig {
+  @Value("${rabbitmq.exchanges.internal}")
+  private String internalExchange;
+  @Value("${rabbitmq.queues.notification}")
+  private String notificationQueue;
+  @Value("${rabbitmq.routing-keys.internal-notification}")
+  private String routingKey;
+}
