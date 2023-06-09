522afe073204b1c46319e7d502809124db97dc30
frank <franktranvantu@gmail.com>
2023-06-09 08:39:55 +0700

    Update

--- notification/src/main/java/com/franktran/notification/NotificationApplication.java
+++ notification/src/main/java/com/franktran/notification/NotificationApplication.java
@@ -2,12 +2,8 @@ package com.franktran.notification;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
-@EnableFeignClients(
-    basePackages = "com.franktran.clients"
-)
 public class NotificationApplication {
   public static void main(String[] args) {
     SpringApplication.run(NotificationApplication.class, args);
