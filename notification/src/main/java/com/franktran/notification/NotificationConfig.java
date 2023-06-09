b808f488d3156aa9ea433c7fc462d27497818da6
frank <franktranvantu@gmail.com>
2023-06-09 23:42:28 +0700

    Update

--- notification/src/main/java/com/franktran/notification/NotificationConfig.java
+++ notification/src/main/java/com/franktran/notification/NotificationConfig.java
@@ -1,7 +1,12 @@
 package com.franktran.notification;
 
 import lombok.Getter;
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.core.TopicExchange;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
@@ -13,4 +18,22 @@ public class NotificationConfig {
   private String notificationQueue;
   @Value("${rabbitmq.routing-keys.internal-notification}")
   private String routingKey;
+
+  @Bean
+  public TopicExchange topicExchange() {
+    return new TopicExchange(internalExchange);
+  }
+
+  @Bean
+  public Binding binding() {
+    return BindingBuilder
+        .bind(queue())
+        .to(topicExchange())
+        .with(routingKey);
+  }
+
+  @Bean
+  public Queue queue() {
+    return new Queue(notificationQueue);
+  }
 }
